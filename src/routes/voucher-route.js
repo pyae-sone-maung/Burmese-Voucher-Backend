b6f7494614215d1d3324fcd742904b7b2e42edf3
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-08-09 12:28:02 +0630

    modify routes

--- src/routes/voucher-route.js
+++ src/routes/voucher-route.js
@@ -4,9 +4,10 @@ const voucherController = require('../controller/voucher-controller')
 
 router.get('/view-voucher/:id', voucherController.viewVoucherById)
 router.get('/balance-voucher', voucherController.showBalanceVouchers)
-router.get('/records', voucherController.showRecordVouchers)
+router.get('/records-voucher', voucherController.showRecordVouchers)
 router.post('/create-voucher', voucherController.createVoucher)
-router.post('/search-voucher', voucherController.searchVoucherByDate)
+router.post('/search-balance-voucher', voucherController.searchBalanceVoucherByDate)
+router.post('/search-record-voucher', voucherController.searchRecordVoucherByDate)
 router.patch('/update-voucher/:id', voucherController.updateVoucherById)
 router.delete('/delete-voucher/:id', voucherController.deleteVoucherById)
 
