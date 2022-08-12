cf7191d9c5ce15c4603cb3768219f4b30b48afcd
pyae-sone-maung <pyaesonemaung.dec@gmail.com>
2022-08-13 00:09:54 +0630

    protect auth middleware

--- src/routes/account-route.js
+++ src/routes/account-route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const accountController = require('../controller/account-controller')
+const { auth } = require('../middleware/auth')
 const router = express.Router()
 
 router.post('/create', accountController.createAccount)
 router.post('/login', accountController.login)
-router.patch('/businessinfo/:id', accountController.updateBusinessInfo)
-router.patch('/account/:id', accountController.updateAccount)
+router.patch('/businessinfo/:id', auth, accountController.updateBusinessInfo)
+router.patch('/account/:id', auth, accountController.updateAccount)
 
 module.exports = router
--- src/routes/voucher-route.js
+++ src/routes/voucher-route.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const voucherController = require('../controller/voucher-controller')
+const { auth } = require('../middleware/auth')
 
-router.get('/view-voucher/:id', voucherController.viewVoucherById)
-router.get('/balance-voucher', voucherController.showBalanceVouchers)
-router.get('/records-voucher', voucherController.showRecordVouchers)
-router.post('/create-voucher', voucherController.createVoucher)
-router.post('/search-balance-voucher', voucherController.searchBalanceVoucherByDate)
-router.post('/search-record-voucher', voucherController.searchRecordVoucherByDate)
-router.patch('/update-voucher/:id', voucherController.updateVoucherById)
-router.delete('/delete-voucher/:id', voucherController.deleteVoucherById)
+router.get('/view-voucher/:id', auth, voucherController.viewVoucherById)
+router.get('/balance-voucher', auth, voucherController.showBalanceVouchers)
+router.get('/records-voucher', auth, voucherController.showRecordVouchers)
+router.post('/create-voucher', auth, voucherController.createVoucher)
+router.post('/search-balance-voucher', auth, voucherController.searchBalanceVoucherByDate)
+router.post('/search-record-voucher', auth, voucherController.searchRecordVoucherByDate)
+router.patch('/update-voucher/:id', auth, voucherController.updateVoucherById)
+router.delete('/delete-voucher/:id', auth, voucherController.deleteVoucherById)
 
 module.exports = router
